0af54a8696a90d1a5f7daac383209212d04a1b76
orijer <65181366+orijer@users.noreply.github.com>
2025-03-17 12:55:39 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from hebrew_w2v_api import HebrewSimilarWords
+import gdown
+import os
+
+if not os.path.exists("./wiki-w2v/words_vectors.npy"):
+    fileID = "13ag2AoEf7s5GUyQMSBJ4HMsuZ8sS_3SL"
+    outputFile = "wiki-w2v/words_vectors.npy"
+    gdown.download(f"https://drive.google.com/uc?id={fileID}", outputFile, quiet=False)
 
 app = FastAPI()
 model = HebrewSimilarWords()
 
-f = open("words.txt", "r")
+f = open("words.txt", "r", encoding="utf-8")
 words = f.readlines()
 f.close()
 
@@ -12,7 +19,7 @@ def get_hidden_word(id):
     if id < 0:
         raise HTTPException(status_code=400, detail="id must be non-negative")
     
-    return words[id % len(words)]
+    return words[id % len(words)].strip()
 
 @app.get("/eval/{id}/{word}")
 def read_root(id: int, word: str):
