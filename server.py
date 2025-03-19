3f0dd7c178c7dfbb3a128d872dcc10ad95a81817
orijer <65181366+orijer@users.noreply.github.com>
2025-03-19 17:35:06 +0200

    Add files via upload
    Added hints to the api, and organized the code a little.

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+import random
 from fastapi import FastAPI, HTTPException
 from hebrew_w2v_api import HebrewSimilarWords
 import gdown
 import os
 
+# SETUP:
 if not os.path.exists("./wiki-w2v/words_vectors.npy"):
     fileID = "13ag2AoEf7s5GUyQMSBJ4HMsuZ8sS_3SL"
     outputFile = "wiki-w2v/words_vectors.npy"
@@ -15,15 +17,39 @@ f = open("words.txt", "r", encoding="utf-8")
 words = f.readlines()
 f.close()
 
+# UTILITY FUNCTIONS:
 def get_hidden_word(id):
     if id < 0:
-        raise HTTPException(status_code=400, detail="id must be non-negative")
+        raise HTTPException(status_code=400, detail="id must be non-negative.")
     
     return words[id % len(words)].strip()
 
-@app.get("/eval/{id}/{word}")
-def read_root(id: int, word: str):
-    hidden_word = get_hidden_word(id)
-    return {"similarity" : model.calc_similarity(hidden_word, word)[2]}
+def get_hint(word):
+    hints = [
+        f"המילה הסודית מתחילה באות '{word[0]}'",
+        f"המילה הסודית מסתיימת באות '{word[-1]}'",
+        f"המילה הסודית מכילה '{len(word)}' אותיות",
+        f"המילה הסודית מכילה את האות '{random.choice(word)}'"
+    ]
+    
+    closest_words = model.get_most_similar(word)[5:]
+    for i in enumerate(len(closest_words)):
+        hints.append(f"המילה '{closest_words[i]}' קרובה למילה הסודית")
+    
+    return random.choice(hints)
 
+# API:
+@app.get("/eval/{id}/{word}")
+def eval_word(id: int, word: str):
+    hidden_word = get_hidden_word(id)
+    try:
+        return {"similarity" : model.calc_similarity(hidden_word, word)[2]}
+    except:
+        raise HTTPException(status_code=400, detail=word+" doesn't exist in the vocabulary.")
+
+@app.get("/hint/{id}")
+def hint(id: int):
+    hidden_word = get_hidden_word(id)
+    return {"hint" : get_hint(hidden_word)}
+    
 # Run command: python -m uvicorn server:app --host 0.0.0.0 --port 8000
